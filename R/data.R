687207e2a30f1fcfe9712ba1af9fba76cb02c9fe
hanel <hanel@fzp.czu.cz>
2017-11-14 22:18:28 +0100

    include correct file with UPOVS (i.e. 1121 UPOVS) - UPOVS_poly

--- R/data.R
+++ R/data.R
@@ -78,7 +78,7 @@ Data = function(...){
          
          'geo' = {
            NADRZE = rgdal::readOGR(file.path(.datadir,"geo/nadrze.shp"), "nadrze")
-           UPOVS = rgdal::readOGR(file.path(.datadir,"geo/UPOV_povodi.shp"), "UPOV_povodi")
+           UPOVS = rgdal::readOGR(file.path(.datadir,"geo/UPOV_poly.shp"))
          },
          
          'qd' = {
